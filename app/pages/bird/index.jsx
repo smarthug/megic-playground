58098417323434736255b7d03afc14b0ab1d4ba2
smarthug <kirklayer@gmail.com>
2024-07-13 09:55:56 +0700

    bird init

---
+++ app/pages/bird/index.jsx
@@ -0,0 +1,31 @@
+import { Canvas } from "@react-three/fiber";
+// import { Experience } from "./components/Experience";
+
+import { OrbitControls } from "@react-three/drei";
+
+export const Experience = () => {
+  return (
+    <>
+      <OrbitControls />
+      <mesh>
+        <boxGeometry />
+        <meshNormalMaterial />
+      </mesh>
+    </>
+  );
+};
+
+function App() {
+  return (
+    <div style={{ width: "100vw", height: "100vh" }}>
+      <Canvas shadows camera={{ position: [3, 3, 3], fov: 30 }}>
+        <color attach="background" args={["#ececec"]} />
+        <Experience />
+      </Canvas>
+    </div>
+  );
+}
+
+export default App;
+
+
---
+++ app/routes/_main.bird.jsx
@@ -0,0 +1,5 @@
+import Bird from "../pages/bird";
+
+export default function TheBird() {
+  return <Bird />;
+}
